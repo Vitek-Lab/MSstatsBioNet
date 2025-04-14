b29dc2d8eb9db1ba20a4deb02a9a9cc825a82a85
Tony Wu <wu.anthon@northeastern.edu>
2025-04-14 18:09:00 -0400

    Add script for getting pathways from INDRA

---
+++ R/getPathwaysFromIndra.R
@@ -0,0 +1,31 @@
+library(tidyverse)
+library(MSstatsBioNet)
+input <- data.table::fread(system.file(
+        "extdata/model.csv",
+        package = "MSstatsBioNet"
+    ))
+
+input = input %>% filter(Label == "DMSO-VTP50469") %>% filter(is.na(issue))
+annotated_df = annotateProteinInfoFromIndra(input, "Uniprot_Mnemonic")
+main_target = "MEN1_HUMAN"
+
+
+
+# Fit a normal distribution to log2FC column values
+library(MASS)
+log2fc_values <- annotated_df$log2FC
+fit <- fitdistr(log2fc_values, "normal")
+para <- fit$estimate
+pnorm(0, mean = para[1], sd = para[2])
+
+# Fit a negative binomial distribution with parameters
+n = 0.5
+p = 0.06
+probability <- dnbinom(2, size = n, prob = p)
+probability
+
+# Call INDRA
+library(httr)
+url = 'https://db.indra.bio/statements/from_agents?subject=7010@HGNC'
+response <- GET(url)
+z = content(response)
