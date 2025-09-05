41b9866c6c693b63343710f1600243cd21dbfb16
Tony Wu <wu.anthon@northeastern.edu>
2025-09-05 15:17:08 -0400

    address coderabbit fix

--- R/visualizeNetworksWithHTML.R
+++ R/visualizeNetworksWithHTML.R
@@ -264,7 +264,7 @@ createEdgeElements <- function(edges) {
                             "', interaction: '", row$interaction,
                             "', edge_type: '", row$edge_type,
                             "', category: '", row$category,
-                            "', evidenceLink: '", row$evidenceLink,
+                            "', evidenceLink: '", gsub("(['\\\\])", "\\\\\\1", ifelse(is.na(row$evidenceLink), "", row$evidenceLink)),
                             "', color: '", style$color,
                             "', line_style: '", style$style,
                             "', arrow_shape: '", style$arrow,
